bfee74b0cb4616a1b7c6049f02a6fa663070334a
Lucy Toman <lucy@toman.me.uk>
2022-06-27 12:03:20 +0200

    adds automatic switch to mailtrap in dev

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -2,28 +2,31 @@ import NextAuth from "next-auth"
 import EmailProvider from "next-auth/providers/email"
 import { PrismaAdapter } from "@next-auth/prisma-adapter"
 import prisma from "lib/prisma"
+let data
+
+if (process.env.NODE_ENV === "development") {
+    data = {
+        server: process.env.EMAIL_SERVER,
+        from: process.env.EMAIL_FROM,
+    }
+} else {
+    data = {
+        server: {
+            host: process.env.EMAIL_SERVER_HOST,
+            port: process.env.EMAIL_SERVER_PORT,
+            auth: {
+                user: process.env.EMAIL_SERVER_USER,
+                pass: process.env.EMAIL_SERVER_PASSWORD,
+            },
+        },
+        from: process.env.EMAIL_FROM,
+    }
+}
+
+console.log(data)
 
 export default NextAuth({
-    // providers: [
-    //     EmailProvider({
-    //         server: process.env.EMAIL_SERVER,
-    //         from: process.env.EMAIL_FROM,
-    //     }),
-    // ],
-
-    providers: [
-        EmailProvider({
-            server: {
-                host: process.env.EMAIL_SERVER_HOST,
-                port: process.env.EMAIL_SERVER_PORT,
-                auth: {
-                    user: process.env.EMAIL_SERVER_USER,
-                    pass: process.env.EMAIL_SERVER_PASSWORD,
-                },
-            },
-            from: process.env.EMAIL_FROM,
-        }),
-    ],
+    providers: [EmailProvider(data)],
 
     database: process.env.DATABASE_URL,
     secret: process.env.SECRET,
